8c57af4f2c8b487ed57c0b4346daa5e3b5630496
Caroline Mestre <carolinemestre930@gmail.com>
2024-10-24 13:21:21 +0000

    feat: Add modularity correction

--- models/intermediate/sales_database/int_sales_database__order.sql
+++ models/intermediate/sales_database/int_sales_database__order.sql
@@ -4,7 +4,7 @@ select order_id,
     sum(total_order_item_amount) as total_order_amount,
     sum(item_quantity) as total_items,
     count(distinct product_id) as total_distinct_items
-from {{ ref('stg_sales_database__order_item') }}
+from {{ ref('int_sales_database__order_item') }}
 group by order_id
 
 ), feedback_grouped_by_order as (
@@ -29,7 +29,7 @@ select o.order_id,
     oi.total_order_amount,
     oi.total_items,
     oi.total_distinct_items
-from {{ ref('stg_sales_database__order') }} as o
+from {{ ref('int_sales_database__order_item') }} as o
 left join order_item_grouped_by_order as oi on o.order_id = oi.order_id
 left join feedback_grouped_by_order as f on f.order_id = o.order_id
 left join {{ ref('stg_sales_database__user' )}} as u on u.user_id = o.user_id
--- models/intermediate/sales_database/int_sales_database__user.sql
+++ models/intermediate/sales_database/int_sales_database__user.sql
@@ -1,48 +1,25 @@
 WITH  orders_summary AS (
 SELECT
- o.user_id,
- SUM(oi.total_order_item_amount) AS total_amount_spent,
- SUM(oi.item_quantity) as total_items,
- COUNT(distinct product_id) as total_distinct_items,
- COUNT(DISTINCT o.order_id) AS total_orders
-FROM {{ ref('stg_sales_database__order_item') }} AS oi
-INNER JOIN
-{{ ref('stg_sales_database__order') }} AS o
-ON oi.order_id = o.order_id
+ user_id,
+ SUM(total_order_amount) AS total_amount_spent,
+ SUM(total_items) as total_items,
+ SUM(total_distinct_items) as total_distinct_items,
+ COUNT(DISTINCT order_id) AS total_orders
+FROM {{ ref('int_sales_database__order') }} 
 GROUP BY
-o.user_id
-),
-
-
-product_summary AS (
-SELECT
-o.user_id,
-oi.product_id,
-ROW_NUMBER() OVER (
-  PARTITION BY o.user_id
-  ORDER BY SUM(item_quantity) DESC
-) AS rn
-FROM
-{{ ref('stg_sales_database__order_item') }} AS oi
-INNER JOIN
-{{ ref('stg_sales_database__order') }} o
-ON oi.order_id = o.order_id
-GROUP BY
-o.user_id,
-oi.product_id
+ user_id
 )
 
 SELECT
 o.user_id,
-u.user_city,
-u.user_state,
-o.total_amount_spent,
-o.total_items,
-o.total_distinct_items,
-o.total_orders,
-p.product_id AS favorite_product_id
-FROM {{ ref('stg_sales_database__user') }} AS u
-INNER JOIN orders_summary o ON u.user_id = o.user_id
-LEFT JOIN product_summary p
+o.user_city,
+o.user_state,
+os.total_amount_spent,
+os.total_items,
+os.total_distinct_items,
+os.total_orders,
+p.favorite_product_id
+FROM {{ ref('int_sales_database__order') }} AS o
+INNER JOIN orders_summary os ON os.user_id = o.user_id
+LEFT JOIN {{ ref('int_sales_database__user_favorite_product') }} p
 ON o.user_id = p.user_id
-AND p.rn = 1
