4a2472b130d5d60c943a953248310c3eea02b410
Caroline Mestre <carolinemestre930@gmail.com>
2024-10-24 13:41:59 +0000

    correct user model

--- models/intermediate/sales_database/int_sales_database__user.sql
+++ models/intermediate/sales_database/int_sales_database__user.sql
@@ -1,13 +1,17 @@
 WITH  orders_summary AS (
 SELECT
  user_id,
+ user_city,
+ user_state,
  SUM(total_order_amount) AS total_amount_spent,
  SUM(total_items) as total_items,
  SUM(total_distinct_items) as total_distinct_items,
  COUNT(DISTINCT order_id) AS total_orders
 FROM {{ ref('int_sales_database__order') }} 
 GROUP BY
- user_id
+ user_id,
+ user_city,
+ user_state
 )
 
 SELECT
@@ -19,7 +23,7 @@ os.total_items,
 os.total_distinct_items,
 os.total_orders,
 p.favorite_product_id
-FROM {{ ref('int_sales_database__order') }} AS o
+FROM orders_summary AS o
 INNER JOIN orders_summary os ON os.user_id = o.user_id
 LEFT JOIN {{ ref('int_sales_database__user_favorite_product') }} p
 ON o.user_id = p.user_id
