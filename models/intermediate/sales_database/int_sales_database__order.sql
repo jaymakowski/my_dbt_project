58c86e209bef11e016e2818833ee1f2b782db44a
Caroline Mestre <carolinemestre930@gmail.com>
2024-10-24 13:34:28 +0000

    correct int_sales_database__order

--- models/intermediate/sales_database/int_sales_database__order.sql
+++ models/intermediate/sales_database/int_sales_database__order.sql
@@ -1,11 +1,19 @@
 with order_item_grouped_by_order as (
 
 select order_id,
+    user_id,
+    order_status,
+    order_created_at,
+    order_approved_at,
     sum(total_order_item_amount) as total_order_amount,
     sum(item_quantity) as total_items,
     count(distinct product_id) as total_distinct_items
 from {{ ref('int_sales_database__order_item') }}
-group by order_id
+group by order_id,
+    user_id,
+    order_status,
+    order_created_at,
+    order_approved_at
 
 ), feedback_grouped_by_order as (
 
@@ -17,19 +25,18 @@ group by order_id
 
 )
 
-select o.order_id,
-    o.user_id,
-    o.order_status,
-    o.order_created_at,
-    o.order_approved_at,
+select oi.order_id,
+    oi.user_id,
+    oi.order_status,
     u.user_city,
     u.user_state,
+    oi.order_created_at,
+    oi.order_approved_at,
     f.sum_feedback_score,
     f.total_feedbacks,
     oi.total_order_amount,
     oi.total_items,
     oi.total_distinct_items
-from {{ ref('int_sales_database__order_item') }} as o
-left join order_item_grouped_by_order as oi on o.order_id = oi.order_id
-left join feedback_grouped_by_order as f on f.order_id = o.order_id
-left join {{ ref('stg_sales_database__user' )}} as u on u.user_id = o.user_id
+from order_item_grouped_by_order as oi 
+left join feedback_grouped_by_order as f on f.order_id = oi.order_id
+left join {{ ref('stg_sales_database__user' )}} as u on u.user_id = oi.user_id
